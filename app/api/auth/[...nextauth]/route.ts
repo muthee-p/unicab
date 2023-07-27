94b051a8dab11547b4d073531e70e56002e5212c
muthee-p <pruemu9@gmail.com>
2023-07-27 17:31:58 +0300

    fixing deployment bugs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ const handler = NextAuth({
   providers: [
     CredentialsProvider({
       
-      email: "Credentials",
+      name: "Credentials",
 
       credentials: {
         email: { label: "email", type: "text", placeholder: "jsmith" },
@@ -27,13 +27,13 @@ const handler = NextAuth({
         const user = await res.json();
 
         if (user) {
-          // Any object returned will be saved in `user` property of the JWT
+         
           return user;
         } else {
-          // If you return null then an error will be displayed advising the user to check their details.
+          
           return null;
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+          
         }
       },
     }),
